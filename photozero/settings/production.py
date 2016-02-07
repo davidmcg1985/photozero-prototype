a04bea7a9801155860691405ce79645ba711375c
davidmcg1985 <dmcgweb101@gmail.com>
2016-02-07 20:05:40 +0000

    changed debug to false

--- photozero/settings/production.py
+++ photozero/settings/production.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
